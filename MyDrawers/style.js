6bcd37503a9c09f76655af4add909676bde744ce
Lucas Perez <47630298+lucasperezweb@users.noreply.github.com>
2021-10-31 18:14:13 +0100

    feat: Sliding Navigation Menu styled

---
+++ MyDrawers/style.js
@@ -0,0 +1,82 @@
+export default {
+    title: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: '18px',
+        height: '40px',
+        marginTop: '10px'
+    },
+    titleContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+    },
+
+    titleImagen: {
+        width: 55,
+        height: 55,
+        borderRadius: 35
+    },
+
+    container: {
+        backgroundColor: '#2C4365',
+        flex: 1,
+    },
+
+    bgContainer: {
+        borderBottomWidth: 0.5,
+        borderBottomColor: 'white'
+    },
+
+
+    camaraContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    camaraIcon: {
+        width: 20,
+        height: 20,
+        position: 'absolute',
+        left: 15,
+        bottom: 3
+    },
+
+    userNombre: {
+        marginVertical: 10,
+    },
+
+    menuContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginLeft: 10,
+        marginVertical: 15,
+        marginTop: 30,
+    },
+
+    iconoContainer: {
+        flex: 1.5,
+        justifyContent: 'center'
+    },
+
+    tituloContainer: {
+        flex: 8.5,
+        justifyContent: 'center',
+    },
+
+    tituloTxt: {
+        color: 'white',
+        fontSize: 15,
+    },
+    difuminado:{
+        flex:1,
+        backgroundColor:'rgba(0, 0, 0, 0.5)'
+    },
+    fondoImagen:{
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0
+    }
+}
