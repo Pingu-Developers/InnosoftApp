38554867b2663dbb3ff21de0b8c05892fb893f2d
Carlos Atienza <caraticar@alum.us.es>
2022-01-03 20:47:56 +0100

    feat: Added swipe back button for iOS devices

--- views/StackAboutUS.js
+++ views/StackAboutUS.js
@@ -10,13 +10,18 @@ import PopUpTransport from './PopUpTransport';
 export default function StackAboutUs() {
   const Stack = createStackNavigator ();
   return (
-      <Stack.Navigator
+      <Stack.Navigator 
       screenOptions={{
           presentation: 'modal',
-          headerShown: false,
+          headerShown: true,
+          headerShadowVisible: false,
+          headerBackTitle:'Volver',
+          headerStyle: {
+            backgroundColor: '#f2f2f2',
+          },
           cardStyleInterpolator: CardStyleInterpolators.forBottomSheetAndroid
         }}>
-          <Stack.Screen name="AboutUs" component={AboutUs} />
+          <Stack.Screen name="AboutUs" options={{headerTintColor:'#f2f2f2'}}  component={AboutUs} />
           <Stack.Screen name="Contactanos" component={Contact} />
           <Stack.Screen name="Como Llegar" component={HowToGetTo} />
           <Stack.Screen name="Organización" component={Organization}/>
