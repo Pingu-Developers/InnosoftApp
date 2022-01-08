a53a7c4a54f07e2fc5c534a123a8857ed5c1a5fd
alesancor1 <alesancor1@alum.us.es>
2022-01-08 17:03:50 +0100

    fix: Fixed config causing env vars not being set

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache(false);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [["inline-dotenv",{ path: '.env', systemVar: 'overwrite' }], 'react-native-reanimated/plugin',]
+    plugins: ["inline-dotenv", 'react-native-reanimated/plugin']
   };
 };
